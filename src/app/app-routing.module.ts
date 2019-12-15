e8dcec92ab11010e4480e3723a1ae417aea1d17b
Aman Sarosh <amansarosh13@gmail.com>
2019-12-14 19:59:17 -0800

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { CardsComponent } from './cards/cards.component';
+import { StepperComponent } from './stepper/stepper.component';
+import { AccordionComponent } from './accordion/accordion.component';
+import { ListComponent } from './list/list.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { TabsComponent } from './tabs/tabs.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'cards', component: CardsComponent },
+  { path: 'stepper', component: StepperComponent },
+  { path: 'accordion', component: AccordionComponent },
+  { path: 'list', component: ListComponent },
+  { path: 'sidebar', component: SidebarComponent },
+  { path: 'tabs', component: TabsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,54 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {
+  NbThemeModule, NbLayoutModule, NbCardModule, NbStepperModule, NbAccordionModule, NbListModule,
+  NbTabsetModule, NbActionsModule, NbInputModule, NbButtonModule, NbRadioModule, NbSelectModule,
+  NbPopoverModule, NbContextMenuModule, NbTooltipModule, NbSidebarModule, 
+} from '@nebular/theme';
+
+import { NbEvaIconsModule } from '@nebular/eva-icons';
+import { HomeComponent } from './home/home.component';
+import { CardsComponent } from './cards/cards.component';
+import { StepperComponent } from './stepper/stepper.component';
+import { AccordionComponent } from './accordion/accordion.component';
+import { ListComponent } from './list/list.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { TabsComponent } from './tabs/tabs.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CardsComponent,
+    StepperComponent,
+    AccordionComponent,
+    ListComponent,
+    SidebarComponent,
+    TabsComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    NbThemeModule.forRoot({ name: 'default' }),
+    NbLayoutModule,
+    NbEvaIconsModule,
+    NbCardModule,
+    NbStepperModule,
+    NbAccordionModule,
+    NbListModule,
+    NbTabsetModule,
+    NbActionsModule,
+    NbInputModule,
+    NbButtonModule,
+    NbRadioModule,
+    NbSelectModule,
+    NbPopoverModule,
+    NbContextMenuModule,
+    NbTooltipModule,
+    NbSidebarModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
